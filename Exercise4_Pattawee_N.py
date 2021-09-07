7eecae2e099203f8650a639baeee00bc990e34da
PattaweeGrean <pattaweemail@gmail.com>
2021-09-07 16:01:24 +0700

    Exercise4_Pattawee_N.py

---
+++ Exercise4_Pattawee_N.py
@@ -0,0 +1,10 @@
+FE = "Foundation English"
+GB = "General Business"
+IC = "Introduction to Computer Systems"
+CP = "Computer Programming"
+
+print("--- Your Score ---")
+print(FE ,"               :", 45.5 ,"points")
+print(GB ,"                 :", 32 ,"  points")
+print(IC ," :", 78.5 ,"points")
+print(CP ,"             :", 65.5 ,"points")
